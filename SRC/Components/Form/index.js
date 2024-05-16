401fc69d7d8ea1b740e1ea8162c16aae6345ef90
DudinhaDaDz7 <eduardamsc27>
2024-05-16 10:26:38 -0300

    ButtonCalcular

--- SRC/Components/Form/index.js
+++ SRC/Components/Form/index.js
@@ -4,9 +4,28 @@ import { View, Text, TextInput, Button } from "react-native";
 export default function Form(){
     const [height, setHeight] = useState(null)
     const [weight, setWeight] = useState(null)
-    const [messageIMC, setmessageIMC] = useState("prencha seu peso e a sua altura");
+    const [messageIMC, setmessageIMC] = useState("Informe seu peso e a sua altura");
     const [imc, setInt] = useState(null)
-    const [textButton, setTextButton] = useState("Calcular IMC")
+    const [textButton, setTextButton] = useState("IMC")
+
+    function imcCalculator(){
+        return setImc((weight/(height*height)).toFixed(2))
+}
+
+    function validationImc(){
+        if(weight != null && height != null){
+            imcCalculator()
+            setHeight(null)
+            setWeight(null)
+            setMessageIMC("SEU IMC É = ")
+            setTextButton("CALCULAR NOVAMENTE")
+            return
+        }
+    
+        setImc(null)
+        setTextButton("calcular")
+        setMessageIMC("preencha Peso e Altura")
+    }
 
     return(
         <View>
@@ -26,7 +45,7 @@ export default function Form(){
             />
 
             <Button
-                title="Qualquer Coisa"
+                title={textButton}
             />
 
         </View>
